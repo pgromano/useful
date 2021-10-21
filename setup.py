e138deccf2308e707af869a3d411a9278832aa02
Pablo Romano <pablo.romano@nytimes.com>
2021-10-21 13:07:12 -0500

    setup.py to allow installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+version = "0.0.0"
+setup(
+    name="useful",
+    author="Pablo Romano",
+    description="Useful functions for Python",
+    version=version,
+    packages=find_packages(),
+    zip_safe=False,
+)
